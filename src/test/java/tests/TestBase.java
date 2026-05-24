3622e40d299c46ea4c00b7e50e42d36141e67ea3
t5582871-svg <dsvredlov@mail.ru>
2026-05-24 11:02:09 +0300

    Update TestBase.java
    Edit TestBase: delete "Configuration.remote"

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -16,7 +16,6 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
